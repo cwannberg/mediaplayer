9033c3cb7ed7088e86557f9bb8469edbfa2ab7a0
Cilla <ceciliawannberg@gmail.com>
2025-08-21 09:49:39 +0200

    Styled slider

--- src/Components/Slider.tsx
+++ src/Components/Slider.tsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
+import '../assets/css/mediaplayer.css'
 
 export default function SliderMock() {
   const [value, setValue] = useState(50);
 
   return (
-    <div className="flex flex-col items-center gap-4 w-full max-w-md">
+    <div>
       <input
+        className="slider"
         type="range"
         min="0"
         max="100"
         value={value}
         onChange={(e) => setValue(Number(e.target.value))}
-        className="w-full"
       />
     </div>
   );
